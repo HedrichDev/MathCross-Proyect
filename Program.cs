892d08569577ff1eecccbb2ac4dbfb67f4afeff4
HΞDЯICHDΞV <christopherhedrich@gmail.com>
2025-05-04 16:42:31 -0400

    cambios

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CodigoFuente
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
         {
+            Console.WriteLine("El número es par.");
+        }
+        else
+        {
+            Console.WriteLine("El número es impar.");
         }
     }
 }
